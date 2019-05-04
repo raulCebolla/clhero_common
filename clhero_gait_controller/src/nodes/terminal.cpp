1a5667d410095b0afbd07166e413a8c34afbec6d
raulCebolla <raulcebolla@gmail.com>
2019-05-04 22:31:19 +0200

    Add files via upload

--- clhero_gait_controller/src/nodes/terminal.cpp
+++ clhero_gait_controller/src/nodes/terminal.cpp
@@ -23,8 +23,17 @@
 
 //Declare the defines needed in the gait pattern
 #define INVALID_COMMAND_ERR 147
+#define EXIT_COMMAND 148
+#define HELP_COMMAND 149
+
 #define REGISTERED_GP_PARAM_NAMESPACE "/clhero/gait_patterns/registered"
 #define CURRENT_GP_PARAM_NAMESPACE "/clhero/gait_patterns/current"
+
+//----------------------------------------------------
+//    Global variables
+//----------------------------------------------------
+
+ros::ServiceClient client;
 
 //----------------------------------------------------
 //    Functions
@@ -40,8 +49,10 @@ int readCommandLine (){
 	std::getline(std::cin, buffer);
 	//std::cin >> buffer;
 
-	if(buffer.compare("q") == 0){
-		return -1;
+	if((buffer.compare("q") == 0)||(buffer.compare("Q") == 0)){
+		return EXIT_COMMAND;
+	}else if((buffer.compare("h") == 0)||(buffer.compare("H") == 0)){
+		return HELP_COMMAND;
 	}else{
 		try{
 			int_buff = std::stoi(buffer, NULL);
@@ -52,11 +63,67 @@ int readCommandLine (){
 	}
 }
 
+bool readYN (){
+	
+	std::string buffer;
+	int int_buff = 0;
+
+	std::getline(std::cin, buffer);
+	//std::cin >> buffer;
+
+	if((buffer.compare("y") == 0)||(buffer.compare("Y") == 0)){
+		return true;
+	}else if((buffer.compare("n") == 0)||(buffer.compare("N") == 0)){
+		return false;
+	}else{
+		std::cout << "Invalid command." << std::endl;
+	}
+
+	return false;
+}
+
+
 void resetRequest (clhero_gait_controller::GaitPatternControl& msg){
 	msg.request.pattern_name.clear();
 	msg.request.args.clear();
 	msg.request.order.clear();
 	return;
+}
+
+bool sendRequest (clhero_gait_controller::GaitPatternControl& msg){
+	
+	std::cout << "Sending request to gait pattern controller" << std::endl;
+	
+	if(client.call(msg)){
+		std::cout << "Message successfully sent." << std::endl;
+		
+		switch(msg.response.ans){
+			case 1:
+				std::cout << "[Error] " << "Order not recognized" << std::endl;
+				break;
+			case 2:
+				std::cout << "[Error] " << "Gait pattern is not in the list" << std::endl;
+				break;
+			case 3:
+				std::cout << "[Error] " << "Gait pattern name does not match with the current active pattern." << std::endl;
+				break;
+			case 4:
+				std::cout << "[Error] " << "Could not change gait pattern at the command interface" << std::endl;
+				break;
+			default:
+				break;
+		}
+
+		return true;
+
+	}else{
+
+		std::cout << "[Error] Could not call server." << std::endl;
+		return false;
+
+	}
+
+	return true;
 }
 
 //----------------------------------------------------
@@ -72,7 +139,7 @@ int main (int argc, char** argv){
 	ros::init(argc, argv, "pattern_control_terminal");
 	ros::NodeHandle nh;
 
-	ros::ServiceClient client = nh.serviceClient<clhero_gait_controller::GaitPatternControl>("gait_pattern_control");
+	client = nh.serviceClient<clhero_gait_controller::GaitPatternControl>("/clhero_gait_control/gait_pattern_control");
 
 	//----------------------------------------------------
 	//    GUI 
@@ -80,11 +147,13 @@ int main (int argc, char** argv){
 
 	bool active = true;
 	bool show_main_menu = true;
+	bool intro_pattern_active = true;
 
 	clhero_gait_controller::GaitPatternControl msg;
 
-	int command;
+	int command, pattern_choice;
 	std::vector<std::string> pattern_names;
+	std::string current_pattern_name;
 
 	std::cout << "------------------------------------------------------------" << std::endl;
 	std::cout << "	Terminal for clhero gait control " << std::endl;
@@ -101,15 +170,15 @@ int main (int argc, char** argv){
 			std::cout << "  [4] Continue paused execution" << std::endl;
 			std::cout << "  [5] Force gait state" << std::endl;
 			std::cout << "  [6] Update movement arguments" << std::endl;
+			std::cout << "  [h] Show main menu" << std::endl;
 			std::cout << "  [q] Exit" << std::endl;
 
 			show_main_menu = false;
 		}
 		
 		std::cout << "------------------------------------" << std::endl;
-		std::cout << "  [Command]: ";
-
-		command = readCommandLine();
+		std::cout << "  [Command]: "; command = readCommandLine();
+		std::cout << "------------------------------------" << std::endl;
 
 		switch (command){
 			case 1:
@@ -125,10 +194,42 @@ int main (int argc, char** argv){
 					break;
 				}
 
-				std::cout << "Avaiable patterns: " << std::endl;
+				std::cout << "Available patterns: " << std::endl;
 
 				for(int i = 0; i < pattern_names.size(); i++){
 					std::cout << "  [" << i+1 << "] " << pattern_names[i] << std::endl;
+				}
+
+				intro_pattern_active = true;
+
+				while(intro_pattern_active){
+
+					std::cout << "Introduce the desired pattern to start (press q to cancel): ";
+					pattern_choice = readCommandLine();
+
+					switch(pattern_choice){
+						case EXIT_COMMAND: 
+							intro_pattern_active = false;
+							break;
+						case HELP_COMMAND:
+							std::cout << "Invalid command." << std::endl;
+							break;
+						case INVALID_COMMAND_ERR:
+							std::cout << "Invalid command." << std::endl;
+							break;
+						default:
+							if((intro_pattern_active < 1) || (intro_pattern_active > pattern_names.size())){
+								std::cout << "Number out of range." << std::endl;
+								break;
+							}else{
+								msg.request.pattern_name = pattern_names[pattern_choice-1];
+								std::cout << "Starting pattern: " << msg.request.pattern_name << std::endl;
+								sendRequest(msg);
+								intro_pattern_active = false;
+								break;
+							}
+					}
+
 				}
 
 				break;
@@ -137,11 +238,37 @@ int main (int argc, char** argv){
 
 				std::cout << "-- " << "Stop gait pattern" << " --" << std::endl;
 
+				msg.request.order = "stop";
+
+				nh.getParam(CURRENT_GP_PARAM_NAMESPACE, current_pattern_name);
+				std::cout << "Current active pattern: " << current_pattern_name << std::endl;
+				std::cout << "Do you want to stop current active pattern? [y/n] ";
+
+				if(readYN()){
+					msg.request.pattern_name = current_pattern_name;
+					sendRequest(msg);
+				}else{
+					std::cout << "Stop cancelled."<< std::endl;
+				}
+
 				break;
 
 			case 3:
 
 				std::cout << "-- " << "Pause gait pattern" << " --" << std::endl;
+
+				msg.request.order = "pause";
+
+				nh.getParam(CURRENT_GP_PARAM_NAMESPACE, current_pattern_name);
+				std::cout << "Current active pattern: " << current_pattern_name << std::endl;
+				std::cout << "Do you want to pause current active pattern? [y/n] ";
+
+				if(readYN()){
+					msg.request.pattern_name = current_pattern_name;
+					sendRequest(msg);
+				}else{
+					std::cout << "Pause cancelled."<< std::endl;
+				}
 
 				break;
 
@@ -149,11 +276,26 @@ int main (int argc, char** argv){
 
 				std::cout << "-- " << "Resume gait pattern" << " --" << std::endl;
 
+				msg.request.order = "continue";
+
+				nh.getParam(CURRENT_GP_PARAM_NAMESPACE, current_pattern_name);
+				std::cout << "Current active pattern: " << current_pattern_name << std::endl;
+				std::cout << "Do you want to resume current active pattern? [y/n] ";
+
+				if(readYN()){
+					msg.request.pattern_name = current_pattern_name;
+					sendRequest(msg);
+				}else{
+					std::cout << "Resume cancelled."<< std::endl;
+				}
+
 				break;
 
 			case 5:
 
 				std::cout << "-- " << "Force state" << " --" << std::endl;
+
+				msg.request.order = "force_state";
 
 				break;
 
@@ -161,15 +303,27 @@ int main (int argc, char** argv){
 
 				std::cout << "-- " << "Update arguments" << " --" << std::endl;
 
+				msg.request.order = "update_args";
+
+				break;
+
+			case HELP_COMMAND:
+
+				show_main_menu = true;
+
+				break;
+
+			case EXIT_COMMAND:
+
+				std::cout << "Exiting terminal process." << std::endl;
+				active = false;
+
 				break;
 
 			default:
-				if(command < 0){
-					std::cout << "Exiting terminal process." << std::endl;
-					active = false;
-				}else{
-					std::cout << "Invalid command." << std::endl;
-				}
+					
+				std::cout << "Invalid command." << std::endl;
+				
 				break;
 		}
 
